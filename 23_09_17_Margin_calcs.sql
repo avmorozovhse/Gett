eedf43aebd0a3acd4fd58a2bf7d6e5de77431121
Alex <avmorozov.hse@yandex.ru>
2017-09-25 14:13:22 +0300

    Update 23_09_17_Margin_calcs.sql

--- 23_09_17_Margin_calcs.sql
+++ 23_09_17_Margin_calcs.sql
@@ -1,3 +1,38 @@
+--Version v1, Alex Morozov's code
+
+SELECT
+  fo.Order_GK,
+  fo.Riding_User_GK,
+  fo.M_Ride_Duration,
+  fo.Customer_Total_Cost_Inc_Vat,
+  fo.Driver_Total_Cost_Inc_Vat,
+  fo.Order_Confirmed_Datetime,
+  dct.Class_Type_Group_Desc,
+  fo.Cancelled_By_Type_Key,
+  fo.Date_Key,
+  fo.Ordering_Corporate_Account_GK,
+  fcdo.Cost_Exc_Vat,
+  fcdo.Cost_Exc_Vat * -1 +
+    CASE WHEN fo.customer_total_cost_inc_vat > fo.driver_total_cost_inc_vat
+      THEN
+        (fo.customer_total_cost_inc_vat - fo.driver_total_cost_inc_vat) / 1.18
+      ELSE
+        (fo.customer_total_cost_inc_vat - fo.driver_total_cost_inc_vat) / 1.00 END as margin_sum
+from dbo.Dwh_Fact_Orders_V fo
+  INNER JOIN dbo.Dwh_Fact_Charging_Drivers_Orders_V fcdo on fcdo.Order_GK = fo.Order_GK
+  INNER JOIN dbo.dwh_dim_charging_components_v dcc on fcdo.Component_Key = dcc.Component_Key
+  INNER JOIN dbo.dwh_dim_class_types_v dct ON dct.class_type_key = fo.class_type_key
+WHERE dcc.Component_Name_Key = 4 
+
+  and fo.Country_Key = 2 
+  and fo.Date_Key > '2017-08-12' 
+  and fo.Date_Key < '2017-08-14'
+
+
+
+
+--Version v1, Dima's code
+
 SELECT
   fo.order_gk,
   fo.customer_total_cost_inc_vat,
@@ -27,3 +62,5 @@ WHERE
   AND dcc.component_name_key = 4
   AND dct.class_type_group_key IN (1, 3)
 ORDER BY fo.order_gk
+
+
